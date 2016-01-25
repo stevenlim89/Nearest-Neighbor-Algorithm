9fd3237d8bae50295b0ddc2f69dc3b92d063fd6f
Steven Lim <stevenlim890@gmail.com>
2016-01-25 03:36:13 -0800

    feature Matrix constructed

--- hw2.cpp
+++ hw2.cpp
@@ -9,15 +9,20 @@ using namespace std;
 int main() {
 
     char delimiter = ' ';
-    vector<string> coordinates;
+
+    // feature vector
     vector<int> feature;
+
+    // Matrix to hold all feature vecttors
     vector< vector<int> > featureMatrix;
 
-    //string lines [];
+    // Variable to hold char number from file
     char line[4];
 
-    char fromFile;
+    // Variable to hold number that used to be a char
     int number;
+
+    // Counter to check when to put into featureMatrix
     int count = 0;
 
     ifstream trainfile; 
@@ -30,27 +35,35 @@ int main() {
         //file, keep reading:
         while (! trainfile.eof() )
         {
-            //place the line from myfile into the
-            //line variable:
-            //getline (myfile,line);
             count++;
+
+            // Get character from file and store in variable line
             trainfile.getline(line, 256, delimiter);
+
+            // line is a character so convert to int
             number = atoi(line);
+
+            // add to feature vector
             feature.push_back(number);
 
+            // If counter is 785, then feature vector has been filled and you can add it to matrix
             if(count == 785){
                 featureMatrix.push_back(feature);
+
+                // reset values
                 count = 0;
+                feature.clear();
             } 
 
-        }  
-        
-        //coordinates = line->System.Split();       
+        }     
     }
 
     trainfile.close();
 
-    cout<< featureMatrix.size() << endl;
+    // Testing purposes
+    for(int i = 0; i < featureMatrix[2].size(); i++){
+        cout<<featureMatrix[2][i] << endl;
+    }
    
     return 0;
 }
