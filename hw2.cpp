dda8d343127f9d4472204f90dad2eeb4ae85e66f
Steven Lim <stevenlim890@gmail.com>
2016-01-24 23:36:45 -0800

    feature matrix made

--- hw2.cpp
+++ hw2.cpp
@@ -1,30 +1,56 @@
 #include <stdio.h>
-#include <ifstream>
-#include <Strings>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
 
-main() {
+using namespace std;
 
-    String delimiter = " ";
-    array<String> coordinates;
+int main() {
 
+    char delimiter = ' ';
+    vector<string> coordinates;
+    vector<int> feature;
+    vector< vector<int> > featureMatrix;
 
-    String lines [];
-    String line;
+    //string lines [];
+    char line[4];
 
+    char fromFile;
+    int number;
+    int count = 0;
 
-    ifstream trainfile ("hw2train.txt");
+    ifstream trainfile; 
+    trainfile.open("hw2train.txt");
 
     //check to see if the file is opened:
-	  if (trainfile.is_open())
-	  {
+    if (trainfile.is_open())
+    {
         //while there are still lines in the
-		//file, keep reading:
+        //file, keep reading:
         while (! trainfile.eof() )
         {
-      	//place the line from myfile into the
-      	//line variable:
-	 	getline (myfile,line);
-   	 
-    	coordinates = line->System.Split();       
-	}
+            //place the line from myfile into the
+            //line variable:
+            //getline (myfile,line);
+            count++;
+            trainfile.getline(line, 256, delimiter);
+            number = atoi(line);
+            feature.push_back(number);
+
+            if(count == 785){
+                featureMatrix.push_back(feature);
+                count = 0;
+            } 
+
+        }  
+        
+        //coordinates = line->System.Split();       
+    }
+
+    trainfile.close();
+
+    cout<< featureMatrix.size() << endl;
+   
+    return 0;
 }
